6333a87d9b6e9edd706addfd11bfc669a2664274
Nightwatch <rdmc0072@yahoo.com>
2021-02-08 15:42:09 -0800

    expanded on new_move_position

--- w3d5_knight/knightpathfinder.rb
+++ w3d5_knight/knightpathfinder.rb
@@ -1,6 +1,6 @@
 class KnightPathFinder
 
-    attr_reader :considered_positions
+    attr_reader :considered_positions, :current_pos, :starting_pos
 
     MOVES = [
         [2, 1],
@@ -14,7 +14,18 @@ class KnightPathFinder
     ]
 
     def self.valid_moves(pos)
+        vm = []
 
+        MOVES.each_with_index() do |el,i|
+            x = pos[0]
+            y = pos[1]
+            x += el[0]
+            y += el[1]
+            vm << [x,y] if x >= 0 && y >= 0 && x <= 7 && y <= 7
+        end
+        p(vm)
+     # valid moves array should contain all possilbe moves from orgin position
+     # if pos was visited, its not a valid move.
     end
 
 
@@ -26,12 +37,19 @@ class KnightPathFinder
     def initialize(starting_pos)
         @considered_positions = [starting_pos] # new position [0,0] - root note   child nodes -[2, 1],[2, -1],[1, 2],[1, -2],[-2, 1],[-2, -1],[-1, 2],[-1, -2]
         #starting position is going to be an x,y coordinate on a chessboard
+        @starting_pos = starting_pos
+        @current_pos = starting_pos
     end
 
+    
     def new_move_position(pos)
-        if KnightPathFinder.valid_moves(pos) != considered_positions
-            considered_positions << pos
-        end
+          test = KnightPathFinder.valid_moves(pos)
+          test.reject() {|el| @considered_positions.include?(el)}
+          test.select() {|el| KnightPathFinder.valid_moves(@starting_pos).include?(el)}
+          #if KnightPathFinder.valid_moves(pos) != considered_positions && KnightPathFinder.valid_moves(@current_pos).include?(pos) 
+            @considered_positions << pos
+            @current_pos = pos
+          end
         
     end
 
@@ -40,4 +58,5 @@ end
 
 kpf = KnightPathFinder.new([0, 0])
 
-p kpf.new_move_position([3, 4])
+#p kpf.new_move_position([3, 4])
+KnightPathFinder.valid_moves([0,0])
