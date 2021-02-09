03cd442507d266f5be476da99a423a6bf859b176
skumtong <sky_kumtong@yahoo.com>
2021-02-09 14:26:15 -0800

    Finish Class Inheritance

--- w4d2/class_inheritance.rb
+++ w4d2/class_inheritance.rb
@@ -26,10 +26,30 @@ class Manager < Employee
         @employees = []
     end
 
-    
+    def salary_tree
+        salaries = 0
+        self.employees.each do |em|
+            if em.is_a?(Manager)
+                salaries += em.salary + em.salary_tree
+            else
+                salaries += em.salary 
+            end
+        end
+
+        salaries
+    end
 
     def bonus(multiplier)
-
+        self.salary_tree * multiplier
     end
 
 end
+
+ned = Manager.new("Ned", 1000000, "Founder")
+darren = Manager.new("Darren", 78000, "TA Manager", ned)
+shawna = Employee.new("Shawna", 12000, "TA", darren)
+david = Employee.new("David", 10000, "TA", darren)
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
