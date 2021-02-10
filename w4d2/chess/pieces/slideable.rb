f04c55e45515cc853ee946d5aaacad3a824924ef
Michael Lyons <michaellyons@Michaels-MBP.bishopdynamics.com>
2021-02-10 09:33:57 -0800

    added notes from the morning lecture

---
+++ w4d2/chess/pieces/slideable.rb
@@ -0,0 +1,11 @@
+#grow_unblocked_moves_in_dir(dx,dy)
+#each invocation of the method goes 1 direction at a time
+#save the positions it can validly move in an array
+#recursive until a piece can't move any more
+    #off the board
+    #can't move through own piece
+    #can't move past an opponent piece
+#call this in moves method 4 times (1 for each direction) (8 times on the queen)
+
+
+#create moves method
---
+++ w4d2/chess/pieces/stepable.rb
@@ -0,0 +1,1 @@
+#move_diffs is like grow_dir method in slideable but only one move away to check if move is valid
