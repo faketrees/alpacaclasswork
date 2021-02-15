deec311d083c25e6d732e532805b01d4ea3b1886
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-02-15 14:34:18 -0800

    Add #insert #include? #remove to ::IntSet

--- w5/w5d1/skeleton/lib/p01_int_set.rb
+++ w5/w5d1/skeleton/lib/p01_int_set.rb
@@ -1,6 +1,6 @@
 class MaxIntSet
 
-  attr_reader :max
+  attr_reader :max, :store
 
   def initialize(max)
     @store = Array.new(max, false)
@@ -8,11 +8,12 @@ class MaxIntSet
   end
 
   def insert(num)
-    raise "Out of bounds" if num > max && num < 0
+    raise "Out of bounds" if num > max || num < 0
     @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
@@ -30,17 +31,23 @@ end
 
 
 class IntSet
+  attr_reader :num_buckets, :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    @store[num % num_buckets] << num
   end
 
   def remove(num)
+    @store[num % num_buckets].delete(num)
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
