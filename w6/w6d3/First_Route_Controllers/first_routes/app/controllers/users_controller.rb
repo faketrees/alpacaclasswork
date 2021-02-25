f0902e19bf333d8b8b20a43b675d584d2d41e321
Michael Lyons <michaellyons@Michaels-MBP.bishopdynamics.com>
2021-02-24 17:12:23 -0800

    FINISHED

--- w6/w6d3/First_Route_Controllers/first_routes/app/controllers/users_controller.rb
+++ w6/w6d3/First_Route_Controllers/first_routes/app/controllers/users_controller.rb
@@ -18,8 +18,25 @@ class UsersController < ApplicationController
     end
 
     def show 
-        #render json: params
+        user = User.find_by(id: params[:id])
+        render json: user
     end
+
+    def update
+       user = User.find_by(id: params[:id])
+        if user.update(user_params)
+            render json: user
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        user.destroy
+        render json: user
+    end
+
 
     private 
     
