9f62d207e228eb2f579b5d7b6eb24572306c6716
aladdin731 <aladdin731@hotmail.com>
2021-02-24 17:04:05 -0800

    Almost

--- w6/w6d3/First_Route_Controllers/first_routes/app/controllers/users_controller.rb
+++ w6/w6d3/First_Route_Controllers/first_routes/app/controllers/users_controller.rb
@@ -2,17 +2,28 @@ class UsersController < ApplicationController
 
     def index
         users = User.all
-
         render json: users
         # render plain: "I'm in the index action!"
     end
 
     def create 
-        render json: params
+        user = User.new(user_params)
+        # replace the `user_attributes_here` with the actual attribute keys
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+
     end
 
     def show 
         #render json: params
     end
 
+    private 
+    
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
 end
