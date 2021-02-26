20bc4f7d06e3ad658371a9bf2dbef1b737b11ba7
Michael Lyons <michaellyons@Michaels-MBP.bishopdynamics.com>
2021-02-26 13:33:29 -0800

    stuff

---
+++ w6/w6d5/notes.rb
@@ -0,0 +1,16 @@
+controllers
+    restful routes
+        a  mapping between HTTP verb + URL path and an action/response
+        -resources  [:index, :new, :create, :show, :edit, :update, :delete]
+                    [get   ,  get,  post, 
+
+add in development tab of gem file
+    gem 'binding_of_caller'
+    gem 'better_errors'
+
+
+    action is a rails path
+    http request is a verb (post, get, etc.) #destroy is not an http request
+
+
+    change_column_null :table_name, :column_name, true
